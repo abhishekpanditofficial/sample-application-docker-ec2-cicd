b5e93a1a61a1409eea0378b3f89a9f97f179eb5e
Abhishek Pandit <abhishekpanditoff@gmail.com>
2024-10-26 16:02:39 +0530

    CI/CD Check

--- index.js
+++ index.js
@@ -4,22 +4,10 @@ const port = process.env.PORT || 80;
 
 // Route for the root URL
 app.get('/', (req, res) => {
-  res.status(200).send('Welcome to the Dummy Server!. Testing the CI/CD pipeline.');
+  res.status(200).send('Welcome to the Dummy Server!. Testing the CI/CD pipeline. Update');
 });
 
 
-app.get('/hello', (req, res) => {
-  res.status(200).send('Hello, there! This is a friendly greeting from the server.');
-});
-
-
-app.get('/about', (req, res) => {
-  res.status(200).json({
-    message: 'About Dummy Server',
-    description: 'This is a simple Express.js server for demonstration purposes.',
-    version: '1.0'
-  });
-});
 
 // Start the server
 app.listen(port, () => {
